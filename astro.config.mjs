c5a268dae1b26f37de1552f2f6b45b07ff12ff6f
Pavel Grinchenko <psd-coder@evilmartians.com>
2024-12-04 16:03:02 +0000

    Setup vercel provider for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,24 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    imagesConfig: {
+      sizes: [320, 480, 640, 750, 828, 1080, 1200],
+    },
+    imageService: true,
+    skewProtection: true,
+    // isr: {
+    //   // A secret random string that you create.
+    //   bypassToken: "",
+    //   // Paths that will always be served fresh.
+    //   exclude: ["/api/invalidate", "/posts/[...slug]"],
+    // },
+  }),
 });
