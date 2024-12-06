2d1fbfa7174c0c18b6908e17524c7a0c0c9fff9d
Pavel Grinchenko <psd-coder@evilmartians.com>
2024-12-06 15:15:04 +0000

    Implement Table base components

---
+++ src/components/Table/index.ts
@@ -0,0 +1,4 @@
+export { default as Table } from "./Table.astro";
+export { default as TableRow } from "./TableRow.astro";
+export { default as TableHeading } from "./TableHeading.astro";
+export { default as TableCell } from "./TableCell.astro";
